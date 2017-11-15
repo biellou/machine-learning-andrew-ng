b2ada39b888950153b3f53421d9fde9fda270554
Oleksii Bielov <oleksii.bielov@zalando.de>
2017-11-15 19:42:59 +0100

    added tasks for week 4

--- ex3/oneVsAll.m
+++ ex3/oneVsAll.m
@@ -55,15 +55,10 @@ X = [ones(m, 1) X];
 	initial_theta = zeros(n + 1, 1);
 	options = optimset('GradObj', 'on', 'MaxIter', 50);
 for c = 1:num_labels
+
 	[theta] = fmincg (@(t)(lrCostFunction(t, X, (y == c), lambda)), initial_theta, options);
-all_theta = [all_theta ; theta'];
+all_theta(c,:) = theta';
 end
-
-
-size(y)
-size(X)
-
-
 
 
 % =========================================================================
