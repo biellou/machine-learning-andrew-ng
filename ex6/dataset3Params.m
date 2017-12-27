440504e3a65c2331a49f6c3d39f6b44e6266d21e
Alexey <alexey.belov@gmail.com>
2017-12-27 12:44:48 +0100

    ex6 completed and ex7 initial

--- ex6/dataset3Params.m
+++ ex6/dataset3Params.m
@@ -11,6 +11,10 @@ function [C, sigma] = dataset3Params(X, y, Xval, yval)
 C = 1;
 sigma = 0.3;
 
+set = [0.01, 0.03, 0.1, 0.3, 1, 3, 10, 30];
+matrix_values = zeros(length(set)^2,2);
+matrix_errors = zeros(length(set)^2,1);
+
 % ====================== YOUR CODE HERE ======================
 % Instructions: Fill in this function to return the optimal C and sigma
 %               learning parameters found using the cross validation set.
@@ -23,11 +27,25 @@ sigma = 0.3;
 %        mean(double(predictions ~= yval))
 %
 
+i = length(set);
+n = 1;
+for C_iter = 1:i
+	for sigma_iter = 1:i
+		model = svmTrain(X, y, set(C_iter), @(x1, x2) gaussianKernel(x1, x2, set(sigma_iter))); 
 
+		predictions = svmPredict(model, Xval);
 
+		pred_error = mean(double(predictions ~= yval));
+		matrix_values(n,1) = set(C_iter);
+		matrix_values(n,2) = set(sigma_iter);
+		matrix_errors(n,1) = pred_error;
+		++n;
+	end
+end
 
-
-
+[minval, row] = min(min(matrix_errors,[],2));
+C = matrix_values(row,1);
+sigma = matrix_values(row,2);
 
 % =========================================================================
 
